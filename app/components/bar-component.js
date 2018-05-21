bd30506ab8f829e0e1c0e07bb2c006a22335147b
Robert Jackson <rjackson@linkedin.com>
2018-05-21 15:16:15 -0400

    Remove work around (demo working state).

--- app/components/bar-component.js
+++ app/components/bar-component.js
@@ -1,12 +1,11 @@
 import Component from '@ember/component';
-import { resolve } from 'rsvp';
 
 export default Component.extend({
     showButtonToggle: false,
 
     actions: {
         onClickButton() {
-            return resolve($.ajax('/authors')).then((data) => {
+            return $.ajax('/authors').then((data) => {
                 console.log(data.data[0].attributes);
                 this.set('buttonData', data.data[0].attributes);
                 this.toggleProperty('showButtonToggle');
